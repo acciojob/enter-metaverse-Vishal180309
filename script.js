04ca68fc9b1a71b547e5470d37189971b5c81df4
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-27 08:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", () => {
+    const statusPara = document.getElementById("status");
+    statusPara.innerText = "Entered Metaverse";
+});
