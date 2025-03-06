7c92e9280a5c74472965707c54f15445bd15e2d4
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-03-06 11:24:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-//your JS code here. If required.
-document.getElementById('enterBtn').addEventListener('click',()=>{
-    const status=document.getElementById('status');
-    if(status.textContent==='Enter the Metaverse'){
-     status.innerHTML=<h1>Entered Metaverse</h1>   
+document.getElementById('enterBtn').addEventListener('click', () => {
+    const status = document.getElementById('status');
+    if (status.textContent === 'Enter the Metaverse') {
+        status.innerHTML = '<h1>Entered Metaverse</h1>';
     }
-    
-
 })
